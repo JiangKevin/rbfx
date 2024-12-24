48992883b402e2dfc9ac80be6e844c0e3c719f87
JiangKevin <JiangKevin@users.noreply.github.com>
2024-12-24 12:22:36 +0800

    备份一下

--- make.sh
+++ make.sh
@@ -9,7 +9,7 @@ export ANDROID_NDK=/Users/kevin.jiang/Dev/tools/AndroidSdk/ndk/26.1.10909125
 # gradle build
 # rm -rf build/*
 cd build
-cmake ..  -DANDROID_STL=c++_static -DANDROID_PLATFORM=android-21 -DANDROID=1 -DBUILD_SHARED_LIBS=OFF -DURHO3D_SAMPLES=OFF -DURHO3D_PLAYER=OFF -URHO3D_EDITOR=OFF -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_NDK=$ANDROID_NDK
+cmake ..  -DANDROID_STL=c++_static -DANDROID_PLATFORM=android-21 -DANDROID=1 -DBUILD_SHARED_LIBS=OFF -DURHO3D_SAMPLES=OFF -DURHO3D_PLAYER=OFF -URHO3D_EDITOR=OFF -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_NDK=$ANDROID_NDK -DURHO3D_NETWORK=OFF
 # 
 make
-make install ./install
+make install ./AndroidInstall
